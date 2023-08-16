81abc25a95ad961536ace24a1889ad3c834fd54d
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-16 18:28:41 +0530

    assigning values to the variable through command line

--- UserVariable.sh
+++ UserVariable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "please enter the username"
-read USERNAME
+read -s USERNAME
 echo "username is $USERNAME"
 echo "please enter the password"
-read PASSWORD
+read -s PASSWORD
 echo "username is $PASSWORD"
 
