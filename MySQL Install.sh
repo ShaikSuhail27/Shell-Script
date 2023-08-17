2c44e18d1f7fa54a88580180a667e1e62d23edc7
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 10:55:57 +0530

    Installing My SQL

--- MySQL Install.sh
+++ MySQL Install.sh
@@ -1,7 +1,10 @@
 #! /bin/bash/
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] # id is not equal to zero then it is normal user or else root root user
 then
-echo "Error:This is not a root user"
+echo "Error:This is not a root user and please go to root access for installing"
+exit 1 #$? is not equal to zero then it is failure 
 fi
+yum install mysql -y
+
