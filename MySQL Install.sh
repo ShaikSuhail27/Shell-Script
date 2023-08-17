4328b4a80e425c008dc07529b1a9255756859532
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 11:32:32 +0530

    Installing My SQL and Postfix

--- MySQL Install.sh
+++ MySQL Install.sh
@@ -6,13 +6,21 @@ then
 echo "Error:This is not a root user and please go to root access for installing"
 exit 1 #$? is not equal to zero then it is failure 
 fi
-yum install mysqll -y
-if [ $? -ne 0 ] 
+yum install mysql -y
+if [ $? -ne 0 ] #To check whether installation is success or not
 then
     echo "There is error in Mysql installation"
     exit 1
 else
 echo "MySql installation is success"
+fi
+yum install postfix -y
+if [ $? -ne 0 ] #To check whether installation is success or not
+then
+    echo "There is error in Postfix installation"
+    exit 1
+else
+echo "Postfix installation is success"
 fi
 
 
