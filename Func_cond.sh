9e7868518c40da8f72a2953be1ca88bc75a5bd3a
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 13:47:50 +0530

    Function and condition task

--- Func_cond.sh
+++ Func_cond.sh
@@ -13,7 +13,7 @@ exit 1 # if any error occurs in shellscript it will not stop by default and we a
 fi
 
 yum install mysqll -y #My sql installation
-if [ $? -ne 0] 
+if [ $? -ne 0 ] 
 then 
 echo "Installation is Failure"
 exit 1
