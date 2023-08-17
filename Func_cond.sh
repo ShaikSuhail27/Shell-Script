d122312d2d9b8e5e7446a70a4c3394b78879e5aa
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 13:46:53 +0530

    Function and condition task

--- Func_cond.sh
+++ Func_cond.sh
@@ -12,4 +12,12 @@ echo "ERROR:please go to root access for any installation purpose"
 exit 1 # if any error occurs in shellscript it will not stop by default and we are responsible to stop, if $? =0(success) or $?=(1-127(error))
 fi
 
-yum install mysql -y #My sql installation
+yum install mysqll -y #My sql installation
+if [ $? -ne 0] 
+then 
+echo "Installation is Failure"
+exit 1
+else
+echo "Installation is Success"
+fi
+
