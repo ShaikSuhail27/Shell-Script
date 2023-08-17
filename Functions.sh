e30a026ff198074700b3767714ee19ce3d5c60e8
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 12:58:38 +0530

    Functions and log files

--- Functions.sh
+++ Functions.sh
@@ -1,6 +1,9 @@
 #! /bin/bash/
 
 USERID=$(id -u)
+DATE=$(date +%F - %H %M %S)
+SCRIPTNAME=$0
+LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
 # this function is used to validate the previous comands status and inform user whether it is successful or not
 VALIDATE (){
     if ($1 -ne 0)
@@ -16,9 +19,9 @@ then
 echo "Error:This is not a root user and please go to root access for installing"
 exit 1 #$? is not equal to zero then it is failure 
 fi
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 VALIDATE $? "MYSQL" 
 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 VALIDATE $? "POSTFIX"
 
