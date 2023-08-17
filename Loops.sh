69ea2526eed1aa9f098d207d58839c2731541f84
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 15:34:11 +0530

    Loops task

--- Loops.sh
+++ Loops.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+VALIDATE() {
+  if [ $1 -ne 0 ]
+  then
+  echo "$2:Installation is failure"
+  else
+  echo "$2:Installation is successful"
+  fi
+}
+
 for packages in $@
 do
 yum install $packages -y
-if  [ $? -ne 0 ] 
-then
-echo "$packages: Installation is failure"
-exit 1
-else
-echo "$packages: Installation is Success"
-fi
+# if  [ $? -ne 0 ] 
+# then
+# echo "$packages: Installation is failure"
+# exit 1
+# else
+# echo "$packages: Installation is Success"
+# fi
+VALIDATE $? $packages
 done
