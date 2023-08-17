e531e1eb55384f3652a0bc474f727a2d005c4b81
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 13:09:58 +0530

    Functions and log files

--- Functions.sh
+++ Functions.sh
@@ -5,13 +5,14 @@ DATE=$(date +%F-%H:%M:%S)
 SCRIPTNAME=$0
 LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
 # this function is used to validate the previous comands status and inform user whether it is successful or not
-VALIDATE () {
-if ($1 -ne 0)
-then
-    echo "$2 Installation is Failure" 
-    exit 1
-else
-    echo "$2 Installation is Success"
+VALIDATE (){
+
+     if [ $1 -ne 0 ]
+     then
+        echo "$2 Installation is Failure" 
+        exit 1
+     else
+        echo "$2 Installation is Success"
 fi
 }
 if [ $USERID -ne 0 ] # id is not equal to zero then it is normal user or else root root user
