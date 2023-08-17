6a1fa7a75d588c4e443bba5f1c865e5d66a43470
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 11:26:20 +0530

    Installing My SQL

--- MySQL Install.sh
+++ MySQL Install.sh
@@ -6,5 +6,12 @@ then
 echo "Error:This is not a root user and please go to root access for installing"
 exit 1 #$? is not equal to zero then it is failure 
 fi
-yum install mysql -y
+yum install mysqll -y
+if [ $? -ne 0 ] then
+echo "There is error in Mysql installation"
+exit 1
+else
+echo "MySql installation is success"
+fi
+
 
