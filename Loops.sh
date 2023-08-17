9009903f0fc21c21db6e20f45fd6396316d68ef1
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 19:55:17 +0530

    Loops task

--- Loops.sh
+++ Loops.sh
@@ -21,7 +21,7 @@ for packages in $@
 do
 if rpm -q "$packages" > /dev/null
 then
-echo -e "$packages $Y is already installed"
+echo -e "$packages $Y is already installed $N"
 else
 yum install $packages -y &>>LOGFILE
 fi
