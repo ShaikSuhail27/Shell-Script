6a780f611c836f06fe2933f59e5e8c48d47c886e
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 13:41:31 +0530

    Function and condition task

--- Func_cond.sh
+++ Func_cond.sh
@@ -9,4 +9,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "ERROR:please go to root access for any installation purpose"
+exit 1 # if any error occurs in shellscript it will not stop by default and we are responsible to stop, if $? =0(success) or $?=(1-127(error))
 fi
+
+yum install mysql -y #My sql installation
