67e905f93c2e1a66d5851076baa06d055d614f00
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-17 13:34:16 +0530

    Function and condition task

---
+++ Func_cond.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#My sql and postfix installation
+
+#to find whether we are in root acess or not
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+echo "ERROR:please go to root access for any installation purpose"
+fi
