47372ef73922723657e440713b97b6c2f29d155a
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-19 00:47:48 +0530

    Delete Log files task

---
+++ Delete-old-logs.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+#To remove the log files more than 14 days
+
+APP_LOG_DIR=/home/centos/app-logs
+DATE=$(date +%F-%H:%M:%S)
+SCRIPTNAME=$0
+LOGDIR=/home/centos/shellscript-logs
+LOGFILE=$LOGDIR/$SCRIPTNAME-$DATE
+
+# Finding the log files inside the directory which is more than 14 days
+
+LOGFILE_TO_FIND =$(find $APP_LOG_DIR -name "*.log" -type f -mtime +14)
+echo "$LOGFILE_TO_FIND"
+
+# reading the file line by line and deleting
+while read line
+do
+echo "deleteing $line" &>> $LOGFILE
+rm -rf $line
+done <<< $LOGFILE_TO_FIND
+
