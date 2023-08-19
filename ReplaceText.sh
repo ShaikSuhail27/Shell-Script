4f41dde4b9fe9fbc7928f8893db1144b5346d012
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-19 20:09:37 +0530

    ReplaceWord Task

--- ReplaceText.sh
+++ ReplaceText.sh
@@ -6,7 +6,5 @@ Input_file=/home/centos/suhail_input.txt
 old-text="SUHAIL"
 new-text="suhail"
 
-while read -r line
-do
-Replace="$(sed -i 's/"$old-text"/"$new-text"/g' $Input_file)"
-done < $Input_file
+Replace=$(sed -i "s/$old-text/$new-text/g" "$Input_file")
+echo "Replacement complete"
