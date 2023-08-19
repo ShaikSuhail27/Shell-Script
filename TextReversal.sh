2d4a391a4c3ffa57907811a4ecbd10478872e2f9
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-19 19:23:02 +0530

    TextReversal Task

--- TextReversal.sh
+++ TextReversal.sh
@@ -3,7 +3,7 @@
 # to reverse the order of characters in each line of a text file
 
 Input_file=/home/centos/suhail_input.txt
-Output_file=/home/centos/suhail_input.txt
+Output_file=/home/centos/suhail_output.txt
 
 while read -r line;
 do 
