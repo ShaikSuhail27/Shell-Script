67b24d7fc44c9330a71a180eddc3b7a46fde79d4
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-19 23:26:52 +0530

    Palindrome Number Task

---
+++ Palindrome_num.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter the number : "
+read NUMBER
+REV_NUM=$(echo $NUMBER | rev)
+if [$NUMBER -eq $REV_NUM ];
+then
+echo "$NUMBER is palindrome"
+else
+echo "$NUMBER is not a palindrome"
+fi
