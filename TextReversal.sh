f32c11d476bad9b829232c68ed6e4f7de1c6b536
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-19 19:18:40 +0530

    Palindrome Task

---
+++ TextReversal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# to reverse the order of characters in each line of a text file
+
+Input_file=/home/centos/suhail_input.txt
+Output_file=/home/centos/suhail_input.txt
+
+while read line
+do 
+reverse=$(echo $line | rev)
+echo "$reverse" >> "$Output_file"
+done < "$Input_file"
+echo "Reversal complete and modified line copied to $Output_file"
