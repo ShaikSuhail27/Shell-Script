1209a7cebade62e33aa8b0f8c54e97b1ad6e4332
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-22 01:46:25 +0530

    Gmail installation

--- Gmail.sh
+++ Gmail.sh
@@ -7,21 +7,25 @@ DATE=$(date +%F-%H:%M:%S)
 SCRIPTNAME=$0
 LOGDIR=/temp/
 LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 # Function to check the status is Success or Failure
 VALIDATE () {
 if [ $1 -ne 0]
 then
-echo "$2 is  failure"
+echo -e "$R $2 is  failure $N"
 else
-echo "$2 is  success"
+echo -e "$G $2 is  success $N"
 fi
 }
 
 #To check whether it is root access or not
 if [ $USERID -ne 0]
 then
-echo "ERROR:please run this with root access"
+echo "$R ERROR:please run this with root access $N"
 fi
 
 yum update -y --exclude=kernel*
@@ -35,6 +39,29 @@ VALIDATE $? "ENABLE" &>> $LOGFILE
 
 systemctl restart postfix 
 VALIDATE $? "RESTART" &>> $LOGFILE
+
+cp Main.cf /etc/postfix/main.cf 
+VALIDATE $? "COPYING" &>> $LOGFILE
+
+touch /etc/postfix/sasl_passwd 
+VALIDATE $? "Creating the file for authentication" &>> $LOGFILE
+
+cp SASL_PASSWD /etc/postfix/sasl_passwd
+VALIDATE $? "adding the user name in file and copying for authentication" &>> $LOGFILE
+
+postmap /etc/postfix/sasl_passwd 
+VALIDATE $? "Postmap" &>> $LOGFILE
+
+echo "This is a test mail & Date $(date)" | mail -s "message" shaiksuhail2767@gmail.com
+VALIDATE $? "sending the mail" &>> $LOGFILE
+
+
+
+
+
+
+
+
 
 
 
