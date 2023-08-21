5c06cf463957b9e035cc73e1a58e0d96fc209d95
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-22 01:50:29 +0530

    Gmail installation

--- Gmail.sh
+++ Gmail.sh
@@ -25,7 +25,7 @@ fi
 #To check whether it is root access or not
 if [ $USERID -ne 0 ] 
 then
-echo -e "$R ERROR:please run this with root access $N" &>> $LOGFILE
+echo -e "$R ERROR:please run this with root access $N" 
 fi
 
 yum update -y --exclude=kernel*
