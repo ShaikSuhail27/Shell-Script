7499d83bd45346baf386ad79d25889a1ef502866
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-21 23:59:36 +0530

    diskspace task

--- Mail.sh
+++ Mail.sh
@@ -2,6 +2,11 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[\/&]/\\&/g' <<< $3)
+echo "escaped content : $BODY"
 ALERT_TYPE=$4
 MESSAGE=$5
+
+#echo $BODY | mail -s $SUBJECT $TO_ADDRESS
+
+FINAL_BODY=$(sed -e 's/Team_name/DevOps Team/g' -e 's/alert/High Disk alert/g' -e "s/Message/$BODY/" MailTemplate.html)
