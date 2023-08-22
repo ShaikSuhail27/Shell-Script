79512e33c0f4ef6415f36ef252c39227e731b276
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-22 08:21:42 +0530

    GMAIL Task

--- Gmail.sh
+++ Gmail.sh
@@ -12,6 +12,12 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
+echo "ENTER THE USERNAME : "
+read USERNAME
+
+echo "ENTER THE password : "
+read PASSWORD
+
 # Function to check the status is Success or Failure
 VALIDATE () {
 if [ $1 -ne 0]
@@ -28,32 +34,32 @@ then
 echo -e "$R ERROR:please run this with root access $N" 
 fi
 
-yum update -y --exclude=kernel*
-VALIDATE $? "UPDATE" &>> $LOGFILE
+yum update -y --exclude=kernel*  &>>$LOGFILE
+VALIDATE $? "UPDATE"
 
-yum -y install postfix cyrus-sasl-plain mailx
-VALIDATE $? "INSTALLATION" &>> $LOGFILE
+yum -y install postfix cyrus-sasl-plain mailx &>>$LOGFILE
+VALIDATE $? "INSTALLATION" 
 
-systemctl enable postfix 
-VALIDATE $? "ENABLE" &>> $LOGFILE
+systemctl enable postfix &>>$LOGFILE
+VALIDATE $? "ENABLE" 
 
-systemctl restart postfix 
-VALIDATE $? "RESTART" &>> $LOGFILE
+systemctl restart postfix &>>$LOGFILE
+VALIDATE $? "RESTART"
 
-cp Main.cf /etc/postfix/main.cf 
-VALIDATE $? "COPYING" &>> $LOGFILE
+cp Main.cf /etc/postfix/main.cf &>>$LOGFILE
+VALIDATE $? "COPYING" 
 
-touch /etc/postfix/sasl_passwd 
-VALIDATE $? "Creating the file for authentication" &>> $LOGFILE
+touch /etc/postfix/sasl_passwd &>>$LOGFILE
+VALIDATE $? "Creating the file for authentication" 
 
-cp SASL_PASSWD /etc/postfix/sasl_passwd
-VALIDATE $? "adding the user name in file and copying for authentication" &>> $LOGFILE
+cp SASL_PASSWD /etc/postfix/sasl_passwd &>>$LOGFILE
+VALIDATE $? "adding the user name in file and copying for authentication" 
 
-postmap /etc/postfix/sasl_passwd 
-VALIDATE $? "Postmap" &>> $LOGFILE
+postmap /etc/postfix/sasl_passwd &>>$LOGFILE
+VALIDATE $? "Postmap" 
 
-echo "This is a test mail & Date $(date)" | mail -s "message" shaiksuhail2767@gmail.com
-VALIDATE $? "sending the mail" &>> $LOGFILE
+echo "This is a test mail & Date $(date)" | mail -s "message" shaiksuhail2767@gmail.com &>>$LOGFILE
+VALIDATE $? "sending the mail" 
 
 
 
