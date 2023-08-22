0bff490250222d3cf67a69049cdd067ec16f050a
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-22 23:44:02 +0530

    GMAIL Task

--- Gmail.sh
+++ Gmail.sh
@@ -53,7 +53,8 @@ VALIDATE $? "RESTART"
 # cp Main.cf /etc/postfix/main.cf &>>$LOGFILE
 #cat Main.cf >> /etc/postfix/main.cf &>>$LOGFILE
 #cat "$MAIN_CONFIG" >> /etc/postfix/main.cf &>>$LOGFILE
-cat "$FILENAME" >> "$MAIN_CONFIG"  &>>$LOGFILE
+#cat "$FILENAME" >> "$MAIN_CONFIG"  &>>$LOGFILE
+echo "$MAIN_CONFIG" >> "$FILENAME" &>>$LOGFILE
 VALIDATE $? "appending" 
 
 touch /etc/postfix/sasl_passwd &>>$LOGFILE
